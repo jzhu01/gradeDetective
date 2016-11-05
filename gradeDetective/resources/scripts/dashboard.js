46879cc5c306bc2229a04601ae6a496fd4afa535
tyarrington <tyarrington2296@gmail.com>
2016-11-05 09:07:57 -0400

    Made edits to dashboard html and javascript

---
+++ gradeDetective/resources/scripts/dashboard.js
@@ -0,0 +1,8 @@
+var main = function(){
+  var points1 = $("#points1").val();
+  var pointsOutOf1 = $("#pointsOutOf1").val();
+  var points2 = $("#points2").val();
+  var pointsOutOf2 = $("#pointsOutOf2").val();
+
+  var grade1 = 
+}
